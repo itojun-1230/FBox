695023d18b6cb4c1339a41a34b7b7a88b6e859c6
DESKTOP-junto <juntou1230@gmail.com>
2023-04-05 17:16:10 +0900

    add: randomArray関数

--- src/array.ts
+++ src/array.ts
@@ -4,3 +4,10 @@ export function initArray(num: number, action: Function) {
         return action(i);
     })
 }
+export function randomArray<T>(array: T[]) { //
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];    //Fisher-Yates
+      }
+      return array;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { initArray } from "./array";
+import { initArray, randomArray } from "./array";
 import { degreeToRadian, radianToDegree } from "./math";
 import { repeat, repeatStep } from "./repeat";
 
 export const GP_Array = {
     initArray,
+    randomArray
 }
 export const GP_repeat = {
     repeat,
