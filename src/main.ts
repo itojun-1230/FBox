4070b95f5e23aea8f3e134279035c5b5c1fb59ed
LAPTOP-junto <juntou1230@gmail.com>
2023-04-05 14:02:10 +0900

    add: repeatStep関数

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-export function repeat(num:number, action:any){
+export function repeat(num:number, action:Function){
     for (let i = 0; i < num; i++) {
         action(i);
-      }
+    }
+}
+
+export function repeatStep(startNum:number, endNum:number, step:number, action:Function){
+    for (let i = startNum; i <= endNum;i += step ){
+        action(i);
+    }
 }
